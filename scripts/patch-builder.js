9e79200d1911f597a687fa2a9a1a0a3861c37cdf
Kruti Comp <kruticomp@gmail.com>
2026-09-11 10:25:48 +0530

    build: add patch-builder script to guarantee reliable NSIS packaging across environments

---
+++ scripts/patch-builder.js
@@ -0,0 +1,16 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const targetFile = path.resolve(__dirname, '../node_modules/app-builder-lib/out/targets/nsis/NsisTarget.js');
+
+if (fs.existsSync(targetFile)) {
+  let content = fs.readFileSync(targetFile, 'utf8');
+  if (content.includes('if ((0, macosVersion_1.isMacOsCatalina)()) {')) {
+    content = content.replace(
+      'if ((0, macosVersion_1.isMacOsCatalina)()) {',
+      'if (true) { // Windows 11 NSIS UninstallerReader patch'
+    );
+    fs.writeFileSync(targetFile, content, 'utf8');
+    console.log('[patch-builder] Successfully applied UninstallerReader patch to NsisTarget.js');
+  }
+}
